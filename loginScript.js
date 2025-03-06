279ec363b18ff3c8206d92ba7817781095b666cd
Hussein Atoui <husseinatoui20@gmail.com>
2025-03-06 15:44:16 +0200

    sign up fixes

--- loginScript.js
+++ loginScript.js
@@ -9,3 +9,12 @@ function goToLogin() {
     document.getElementById("loginForm").style.display = "flex";
     document.getElementById("signupForm").style.display = "none";
 }
+
+document.getElementById("profileImage").addEventListener("change", function () {
+    let file = this.files[0];
+    if (file) {
+        document.getElementById("fileName").textContent = file.name; // Show file name
+    } else {
+        document.getElementById("fileName").textContent = "No file chosen";
+    }
+});
