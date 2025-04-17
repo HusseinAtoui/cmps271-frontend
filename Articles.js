76af6f32e95a0445d802b97095d2d7dca4d51767
Bouthaina <bouthainahhk@outlook.com>
2025-04-17 16:17:38 +0300

    Revert "1-2-3-4"
    This reverts commit 0cdb608903dc8e437e8f6d93f39c469664b1649a.

--- Articles.js
+++ Articles.js
@@ -224,7 +224,7 @@ function setupHeartButton(articleId) {
 
     try {
       const response = await fetch(
-        `https://afterthoughts.onrender.com/api/articles/1/2/3/4/${articleId}/like-status`,
+        `https://afterthoughts.onrender.com/api/articles/${articleId}/like-status`,
         {
           headers: { Authorization: `Bearer ${token}` }
         }
